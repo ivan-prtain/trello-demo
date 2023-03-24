7979c1edba5b7d3609d94a3043d127a2314388b8
Ivan Prtain <ivan.prtain@borealis.biz>
2023-03-24 18:46:14 +0100

    Initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BoardsListComponent } from './components/boards-list/boards-list.component';
+import { BoardComponent } from './components/board/board.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: BoardsListComponent },
+  { path: 'board/:id', component: BoardComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardsListComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/boards-list/boards-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards-list',
+  templateUrl: './boards-list.component.html',
+  styleUrls: ['./boards-list.component.scss']
+})
+export class BoardsListComponent {
+
+}
